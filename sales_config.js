ade4c53edebc5e31f39bf90b2b7c7224455aa16a
HKWT <guanhuachen74@gmail.com>
2026-01-06 15:05:14 +0800

    Update sales_config.js

--- sales_config.js
+++ sales_config.js
@@ -4,6 +4,7 @@ const salesTeam = {
     "B": { name: "博士", phone: "63778262" },
     "C": { name: "嫦姐", phone: "98233606" },
     "D": { name: "David", phone: "97944983" }
+    "E": { name: "二佬", phone: "51880888" }
 };
 
 // 預設銷售 (如果網址沒帶代號，就用這個)
