18112db37a5c6588ccc3d47d0a841e9b25973431
HKWT <guanhuachen74@gmail.com>
2026-01-06 15:28:59 +0800

    Update sales_config.js

--- sales_config.js
+++ sales_config.js
@@ -1,9 +1,26 @@
 // sales_config.js - 銷售人員通訊錄
+
 const salesTeam = {
-    "A": { name: "志哥", phone: "63771688" }, // 請改成真實名字和電話
-    "B": { name: "博士", phone: "63778262" },
-    "C": { name: "嫦姐", phone: "98233606" },
-    "D": { name: "David", phone: "97944983" }
+    "A": {
+        name: "志哥",
+        phone: "63771688"
+    },
+    "B": {
+        name: "博士",
+        phone: "63778262"
+    },
+    "C": {
+        name: "嫦姐",
+        phone: "98233606"
+    },
+    "D": {
+        name: "David",
+        phone: "97944983"
+    },
+    "E": {
+        name: "二佬",
+        phone: "51880888"
+    }
 };
 
 // 預設銷售 (如果網址沒帶代號，就用這個)
